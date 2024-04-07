5cf5376bcaa038205b45bde6506b359fe4bd4a1b
swamy <ashokitschool@gmail.com>
2024-04-07 18:58:16 +0300

    initialok commit
    `#	new file:   src/.DS_Store

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -8,7 +8,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class Application {
 
+<<<<<<< HEAD
 	int i =10;
+=======
+	String s = 'mike';
+>>>>>>> 0ffe2c9 (initial commit)
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
