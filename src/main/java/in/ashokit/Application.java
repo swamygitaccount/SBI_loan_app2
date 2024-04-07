f6317793bbf12b0e087899d68487a4820232f729
swamygitaccount <113229573+swamygitaccount@users.noreply.github.com>
2024-04-07 18:55:30 +0300

    Update Application.java

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class Application {
 
+	int i =10;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
